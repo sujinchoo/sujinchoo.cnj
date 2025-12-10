b9bfea6d6343d6266810fc96adec99e0aef3d6ee
Chubie <40617079+sujinchoo@users.noreply.github.com>
2025-12-10 23:36:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -61,9 +61,8 @@ def create_app():
 
     return app
 
-
+app = create_app()
 if __name__ == "__main__":
-    app = create_app()
     with app.app_context():
         db.create_all()
     app.run(debug=True)
